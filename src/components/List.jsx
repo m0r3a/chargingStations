b0d615d8d7cce69890574f5a21b5259460088de0
Psenicinii Valerian <pseniciniivalerian@gmail.com>
2023-11-04 16:58:41 +0200

    Changed the overall map position on screen

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function List({ coordinates, toggleStatus }) {
   return (
     <div className="list">
+      <p className="station-status-msg">Stations Status:</p>
       <p id="list-item">
         {coordinates.map((coord) => (
           <p key={coord.id}>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,50 +1,51 @@
-import React, { useState } from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
-import initialCoordinates from './initialCoordinates';
-import List from './List';
+import React, { useState } from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import initialCoordinates from "./initialCoordinates";
+import List from "./List";
 
 const redMarkerIcon = {
-  url: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png', // URL for the red marker icon
+  url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png", // URL for the red marker icon
 };
 
 const greenMarkerIcon = {
-  url: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png', // URL for the green marker icon
+  url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png", // URL for the green marker icon
 };
-
 
 const MapComponent = (props) => {
   const mapStyles = {
-    height:'700px',
-    width: '80%'
+    height: "95%",
+    width: "80%",
+    marginTop: "22px",
   };
 
   const [coordinates, setCoordinates] = useState(initialCoordinates);
 
   const toggleStatus = (index) => {
     const updatedCoordinates = [...coordinates];
-    updatedCoordinates[index].status = coordinates[index].status === 'on' ? 'off' : 'on';
+    updatedCoordinates[index].status =
+      coordinates[index].status === "on" ? "off" : "on";
     setCoordinates(updatedCoordinates);
   };
 
   return (
     <div>
-      <Map 
-        id='center'
+      <Map
+        id="center"
         google={props.google}
         zoom={14} // Set the initial zoom level to 13
         style={mapStyles}
         initialCenter={{
-          lat: 47.0530, // Oradea city latitude
+          lat: 47.053, // Oradea city latitude
           lng: 21.9282, // Oradea city longitude
         }}
       >
         {coordinates.map((coord, index) => (
           <Marker
-          key={index}
-          position={coord}
-          icon={coord.status === 'on' ? greenMarkerIcon : redMarkerIcon}
-          onClick={() => toggleStatus(index)}
-        />
+            key={index}
+            position={coord}
+            icon={coord.status === "on" ? greenMarkerIcon : redMarkerIcon}
+            onClick={() => toggleStatus(index)}
+          />
         ))}
       </Map>
       <List coordinates={coordinates} toggleStatus={toggleStatus} />
@@ -53,5 +54,5 @@ const MapComponent = (props) => {
 };
 
 export default GoogleApiWrapper({
-  apiKey: '', // Replace with your own Google Maps API key
+  apiKey: "", // Replace with your own Google Maps API key
 })(MapComponent);
