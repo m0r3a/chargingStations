d977b8a6af1cc3d733063cec97434ec39724361e
Psenicinii Valerian <pseniciniivalerian@gmail.com>
2023-11-04 15:27:45 +0200

    Changed status position on the screen

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 function List({ coordinates, toggleStatus }) {
   return (
-    <div id='list'>
-      {coordinates.map((coord) => (
-        <div key={coord.id}>
-          id: {coord.id} | status: {coord.status}
-          <button onClick={() => toggleStatus(coord.id)}>
-          </button>
-        </div>
-      ))}
+    <div className="list">
+      <p id="list-item">
+        {coordinates.map((coord) => (
+          <p key={coord.id}>
+            id: {coord.id} | status: {coord.status}
+            <button onClick={() => toggleStatus(coord.id)}></button>
+          </p>
+        ))}
+      </p>
     </div>
   );
 }
